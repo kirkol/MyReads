d2a09b37ed0f493ac178c07d69538ddace71515d
kirkol <antonina.kirkalo@gmail.com>
2018-07-04 07:05:51 +0000

    2 notes remained

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,29 +1,26 @@
-import React, {Component} from 'react'
+import React from 'react'
 import Selector from './Selector'
 
-class Book extends Component {
-
-  render(){
+function Book(props) {
     return(
       <div className="book">
         <div className="book-top">
-          {(this.props.book.imageLinks)
+          {(props.book.imageLinks)
           ?(
-          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${this.props.book.imageLinks.thumbnail})` }}></div>
+          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${props.book.imageLinks.thumbnail})` }}></div>
           ):(
           <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url()` }}></div>
           )}
           <Selector 
-            shelfs={this.props.shelfs}
-            update={this.props.update}
-            book={this.props.book}
+            shelfs={props.shelfs}
+            update={props.update}
+            book={props.book}
             />
         </div>
-        <div className="book-title">{this.props.book.title}</div>
-        <div className="book-authors">{this.props.book.authors}</div>
+        <div className="book-title">{props.book.title}</div>
+        <div className="book-authors">{props.book.authors}</div>
       </div>
     )
-  }
 }
 
 export default Book
